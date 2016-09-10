32691a3caefc68b5979f80b7e5f7a7785cfe799d
Alejosd <alejosd5@gmail.com>
2016-09-10 18:55:34 -0300

    app actualizada en play store codeVersion 2

--- app/src/main/java/com/tumblr/alejosd5/featuretoggle/activities/MainActivity.java
+++ app/src/main/java/com/tumblr/alejosd5/featuretoggle/activities/MainActivity.java
@@ -13,9 +13,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.tumblr.alejosd5.featuretoggle.R;
 import com.tumblr.alejosd5.featuretoggle.rest.MessageRest;
 
